c6521c41f630cc195e1cf6ccedf597c8e6ba501a
khmalaa <119732582+khmalaa@users.noreply.github.com>
2023-02-06 14:03:54 +0400

    Update index.js

--- index.js
+++ index.js
@@ -81,3 +81,41 @@ buttonMultiply.addEventListener("click", function () {
 buttonDivide.addEventListener("click", function () {
   display.value = display.value + "/";
 });
+
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "0") {
+    display.value = display.value + "0";
+  } else if (e.key === "1") {
+    display.value = display.value + "1";
+  } else if (e.key === "2") {
+    display.value = display.value + "2";
+  } else if (e.key === "3") {
+    display.value = display.value + "3";
+  } else if (e.key === "4") {
+    display.value = display.value + "4";
+  } else if (e.key === "5") {
+    display.value = display.value + "5";
+  } else if (e.key === "6") {
+    display.value = display.value + "6";
+  } else if (e.key === "7") {
+    display.value = display.value + "7";
+  } else if (e.key === "8") {
+    display.value = display.value + "8";
+  } else if (e.key === "9") {
+    display.value = display.value + "9";
+  } else if (e.key === "*") {
+    display.value = display.value + "*";
+  } else if (e.key === "/") {
+    display.value = display.value + "/";
+  } else if (e.key === "+") {
+    display.value = display.value + "+";
+  } else if (e.key === "-") {
+    display.value = display.value + "-";
+  } else if (e.key === "Enter") {
+    display.value = eval(display.value);
+  } else if (e.key === "c") {
+    display.value = "";
+  }
+});
+
